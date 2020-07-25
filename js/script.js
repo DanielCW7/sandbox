d85f460440d526e6dc701155419a66e9207da59a
daniel <57467819+DanielCW7@users.noreply.github.com>
2020-07-25 16:47:29 -0500

    Add files via upload
    initial upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var html = ' ';
+// for (var i = 1; i <= 40; i += 1) {
+//     html += '<div>' + i + '</div>';
+// }
+
+// alert(html);
+
+// this^ [for] loop creates a list of numbers all the way up to 40 and writes them to the html.
+// use the <script> tag to link it to the html page. <script src="js/script.js"></script>
+
+
+
+
+
